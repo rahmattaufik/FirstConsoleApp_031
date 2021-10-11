df8fd93febea80da30b4244c9a1803dfaba9a98e
rahmattaufik <34697741+rahmattaufik@users.noreply.github.com>
2021-10-11 23:05:27 +0700

    Add files via upload

---
+++ FirstConsoleApp_031/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace FirstConsoleApp_031
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("RahnatTaufik_031 | FirstConsoleApp");
+
+            Console.ReadKey();
+        }
+    }
+}
